009acf6673011da696261ca5300275cca3918dcb
ghghghg2 <sam7793085@gmail.com>
2022-01-10 23:05:57 +0800

    Add exercise of argv and envp

---
+++ exercise/argv_and_envp/myecho.c
@@ -0,0 +1,27 @@
+/*
+ Write a program called myecho that prints its command line arguments and environment variable
+*/
+
+#include "stdlib.h"
+#include "stdio.h"
+
+int main (int argc, char **argv, char **envp)
+{
+    int i = 0; 
+
+    printf("Command Line arguments: \n");
+    while (argv[i] != NULL) {
+        printf("argv[%d] = %s \n", i, argv[i]);
+        i++;
+    }
+
+    i = 0;
+
+    printf("Environment Variables: \n");
+    while (envp[i] != NULL) {
+        printf("envp[%d] = %s \n", i, argv[i]);
+        i++;
+    }
+    
+    exit(EXIT_SUCCESS);
+}
