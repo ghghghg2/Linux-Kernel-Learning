634b6ce5b8f2c68d2c1572ae2c24c7ebaf1d3c5e
ghghghg2 <sam7793085@gmail.com>
2022-02-16 22:12:16 +0800

    Change directories

---
+++ compiler_drive/static_lib_archive/my_vector.h
@@ -0,0 +1,10 @@
+
+
+// add the x and y (size n) in element-wise way
+// put the result vector to z
+void addvec(int *x, int *y, int *z, int n);
+
+
+// multiply the x and y (size n) in element-wise way
+// put the result vector to z
+void multvec(int *x, int *y, int *z, int n);
